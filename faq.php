bd46c83ac256ba54f320d5f0e0534930bb67895d
Thamalpath Sathimantha <thamalpathsathimantha3@gmail.com>
2024-02-17 21:05:57 +0530

    Added

---
+++ faq.php
@@ -0,0 +1,106 @@
+<?php include('partials/header.php'); ?>
+
+<main>
+   
+ <!-- Breadcrumb area start --> 
+<div class="breadcrumb__area theme-bg-1 p-relative pt-160 pb-160">
+   <div class="breadcrumb__thumb" data-background="assets/imgs/resources/page-title-bg-1.jpg"></div>
+   <div class="breadcrumb__thumb_3" data-background="assets/imgs/shapes/shape-53.png"></div>
+   <div class="small-container">
+      <div class="row justify-content-center">
+         <div class="col-xxl-12">
+            <div class="breadcrumb__wrapper p-relative">
+               <h2 class="breadcrumb__title">Faq</h2>
+               <div class="breadcrumb__menu">
+                  <nav>
+                     <ul>
+                        <li><span><a href="index.php">Home</a></span></li>
+                        <li><span>Faq</span></li>
+                     </ul>
+                  </nav>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+ <!-- Breadcrumb area end --> 
+
+ <section class="faq-page-section section-space">
+   <div class="small-container">
+      <div class="row">
+         <div class="col-xxl-7 col-xl-7 col-lg-7">
+            <div class="faq-wrapper pr-80">
+               <div class="title-box mb-25 wow fadeInLeft" data-wow-delay=".5s">
+                  <span class="section-sub-title no-border">FAQ</span>
+                  <h3 class="section-title mt-10">Frequently Asked Questions?</h3>
+               </div>
+               <div class="bd-faq">
+                  <div class="accordion" id="accordionExample-st-2">
+                     <div class="bd-faq-group">
+                        <div class="accordion-item">
+                           <h2 class="accordion-header" id="headingOne-st-2">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne-st-2" aria-expanded="false" aria-controls="collapseOne-st-2">
+                                How long does it take to professional to reach?
+                              </button>
+                           </h2>
+                           <div id="collapseOne-st-2" class="accordion-collapse collapse" aria-labelledby="headingOne-st-2" data-bs-parent="#accordionExample-st-2">
+                              <div class="accordion-body">
+                                It depends on the distance between you and the professional. It can take up to 2 days to reach the professional.
+                              </div>
+                           </div>
+                        </div>
+                        <div class="accordion-item">
+                           <h2 class="accordion-header" id="headingTwo-st-2">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo-st-2" aria-expanded="false" aria-controls="collapseTwo-st-2">
+                                What is the minimum budget for the service?
+                              </button>
+                           </h2>
+                           <div id="collapseTwo-st-2" class="accordion-collapse collapse" aria-labelledby="headingTwo-st-2" data-bs-parent="#accordionExample-st-2" style="">
+                              <div class="accordion-body">
+                                It depends on the service you want to fix. It can be between ₹1000 to ₹5000.
+                              </div>
+                           </div>
+                        </div>
+                        <div class="accordion-item">
+                           <h2 class="accordion-header" id="headingThree-st-2">
+                              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree-st-2" aria-expanded="true" aria-controls="collapseThree-st-2">
+                                What is experience level of the professional?
+                              </button>
+                           </h2>
+                           <div id="collapseThree-st-2" class="accordion-collapse collapse show" aria-labelledby="headingThree-st-2" data-bs-parent="#accordionExample-st-2" style="">
+                              <div class="accordion-body">
+                                They are highly skilled and particular about their professional. They have a minimum of 3 years of experience.
+                              </div>
+                           </div>
+                        </div>
+                        <div class="accordion-item">
+                           <h2 class="accordion-header" id="headingFour-st-2">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour-st-2" aria-expanded="false" aria-controls="collapseFour-st-2">
+                                How does refund work?
+                              </button>
+                           </h2>
+                           <div id="collapseFour-st-2" class="accordion-collapse collapse" aria-labelledby="headingFour-st-2" data-bs-parent="#accordionExample-st-2">
+                              <div class="accordion-body">
+                                If your are not satisfied with the service, you can cancel the service and get the money back.                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-xxl-5 col-xl-5 col-lg-5">
+            <figure class="w-img pt-15">
+               <img src="assets/imgs/resources/faq-2.jpg" alt="">
+            </figure>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+ </main>  
+
+ <!-- Footer area start -->
+<?php include('partials/footer.php'); ?>
